8efecd5d002acf7f950201dad7a191a35acad738
ChCh1999 <2518663435@qq.com>
2020-04-21 22:56:03 +0800

    全部订单与删除订单

--- bookShop/src/main/java/edu/whu/bookshop/controller/OrderController.java
+++ bookShop/src/main/java/edu/whu/bookshop/controller/OrderController.java
@@ -20,55 +20,72 @@ public class OrderController {
     @Autowired
     DataTool dataTool;
 
-    @RequestMapping(value = "/myorder",method = RequestMethod.GET)
-    public Object getMyOrder(HttpServletRequest request){
+    @RequestMapping(value = "/myorder", method = RequestMethod.GET)
+    public Object getMyOrder(HttpServletRequest request) {
         Map u = (Map) SessionHelper.getSession(request.getRequestedSessionId());
-        Map<String,Object> param = new HashMap<>();
-        param.put("userAccount",u.get("account"));
+        Map<String, Object> param = new HashMap<>();
+        param.put("userAccount", u.get("account"));
         List<order> orders = dataTool.searchOrder(param);
         List<Object> results = new ArrayList<>();
-        for(order o : orders){
+        for (order o : orders) {
             int bookID = o.getBookID();
-            Map<String,Object> param2 = new HashMap();
-            param2.put("id",bookID);
+            Map<String, Object> param2 = new HashMap();
+            param2.put("id", bookID);
             List<book> books = dataTool.searchBook(param2);
             Map mp = o.genDataMap();
-            if(books!=null && books.size() > 0){
-                mp.put("bookInfo",books.get(0));
+            if (books != null && books.size() > 0) {
+                mp.put("bookInfo", books.get(0));
             }
             results.add(mp);
         }
         return results;
     }
 
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public Object getOrder(HttpServletRequest request) {
+        List<order> orders = dataTool.searchOrder(null);
+        List<Object> results = new ArrayList<>();
+        for (order o : orders) {
+            int bookID = o.getBookID();
+            Map<String, Object> param2 = new HashMap();
+            param2.put("id", bookID);
+            List<book> books = dataTool.searchBook(param2);
+            Map mp = o.genDataMap();
+            if (books != null && books.size() > 0) {
+                mp.put("bookInfo", books.get(0));
+            }
+            results.add(mp);
+        }
+        return results;
+    }
 
-    @RequestMapping(value = "/addbook",method = RequestMethod.POST)
+    @RequestMapping(value = "/addbook", method = RequestMethod.POST)
     public String addBook(HttpServletRequest request, HttpServletResponse response
-            ,@RequestBody Map data){
+            , @RequestBody Map data) {
         try {
             Map orderMap = new HashMap();
-            orderMap.put("bookID",data.get("bookid"));
-            orderMap.put("count",data.get("count"));
+            orderMap.put("bookID", data.get("bookid"));
+            orderMap.put("count", data.get("count"));
             Map session = (Map) SessionHelper.getSession(request.getRequestedSessionId());
-            orderMap.put("userAccount",session.get("account"));
+            orderMap.put("userAccount", session.get("account"));
             order o = new orderBuilder().build(orderMap);
-            if(dataTool.insertOrder(o)){
+            if (dataTool.insertOrder(o)) {
                 return "success";
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         return "failed";
     }
 
-    @RequestMapping(value = "/deletebook",method = RequestMethod.POST)
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public String deleteBook(HttpServletRequest request, HttpServletResponse response
-            ,@RequestBody Map data) {
+            , @RequestBody Map data) {
         try {
             dataTool.deleteOrderByID(Integer.parseInt((String) data.get("orderid")));
             return "success";
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
